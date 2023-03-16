0794b3eaebed759726628f203697809701c06f7f
Valerie K <valerik210695@gmail.com>
2023-03-16 12:47:08 +0300

    example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import UserList from "./components/UserList";
+import TodoList from "./components/TodoList";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div>
+      <UserList/>
+      <hr/>
+      <TodoList/>
     </div>
   );
-}
+};
 
 export default App;
